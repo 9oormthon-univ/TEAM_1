34126ac04f8831abfc47b08c49d0b3565d780475
csct3434 <csct3434@gmail.com>
2023-11-13 01:06:42 +0900

    chore: 스프링 부트 프로젝트 생성

---
+++ src/main/java/today/seasoning/seasoning/SeasoningApplication.java
@@ -0,0 +1,13 @@
+package today.seasoning.seasoning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasoningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasoningApplication.class, args);
+	}
+
+}
