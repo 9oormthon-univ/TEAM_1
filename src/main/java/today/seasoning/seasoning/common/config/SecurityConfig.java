827ba80257df7400d62eb143675ab95e3b678b01
csct3434 <csct3434@gmail.com>
2023-11-13 08:07:49 +0900

    feat: Spring Security 및 CORS 설정

---
+++ src/main/java/today/seasoning/seasoning/common/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package today.seasoning.seasoning.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors().and();
+        return httpSecurity.build();
+    }
+}
