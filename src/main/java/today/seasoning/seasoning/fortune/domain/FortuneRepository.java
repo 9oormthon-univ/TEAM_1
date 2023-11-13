cbfb6db680b98311c7d1f94a2e95cb525c21df7e
linavell <linavell@naver.com>
2023-11-14 01:12:01 +0900

    feat: 오늘의 운세 리포지토리 작성

---
+++ src/main/java/today/seasoning/seasoning/fortune/domain/FortuneRepository.java
@@ -0,0 +1,7 @@
+package today.seasoning.seasoning.fortune.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FortuneRepository extends JpaRepository<Fortune, Long> {
+
+}
