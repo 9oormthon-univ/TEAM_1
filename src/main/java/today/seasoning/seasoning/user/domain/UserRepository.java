e9f475163ee8b198d14856ae59496aa577845198
csct3434 <csct3434@gmail.com>
2023-11-13 20:45:13 +0900

    feat: 회원 리포지터리 작성

---
+++ src/main/java/today/seasoning/seasoning/user/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package today.seasoning.seasoning.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
