5542237098950beae816b9df0a8981f1d8393685
csct3434 <csct3434@gmail.com>
2023-11-13 10:24:49 +0900

    feat: JWT 필터 구현

---
+++ src/main/java/today/seasoning/seasoning/common/config/JwtFilter.java
@@ -0,0 +1,76 @@
+package today.seasoning.seasoning.common.config;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import today.seasoning.seasoning.common.UserPrincipal;
+import today.seasoning.seasoning.common.util.JwtUtil;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+	private final JwtUtil jwtUtil;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws IOException, ServletException {
+
+		String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+		if (authorization == null || !authorization.startsWith("Bearer ")) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰 누락");
+			return;
+		}
+
+		String token = authorization.split(" ")[1];
+
+		try {
+			UserPrincipal userPrincipal = createPrincipalFromToken(token);
+
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+				userPrincipal, null, List.of(new SimpleGrantedAuthority("USER")));
+
+			authenticationToken.setDetails(
+				new WebAuthenticationDetailsSource().buildDetails(request));
+
+			SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+		} catch (ExpiredJwtException e) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰 만료");
+			return;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "토큰 오류");
+			return;
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		List<String> excludePath = Arrays.asList("/oauth/kakao/login");
+
+		String path = request.getRequestURI();
+
+		return excludePath.stream().anyMatch(path::equals);
+	}
+
+	private UserPrincipal createPrincipalFromToken(String token) {
+		return new UserPrincipal(
+			jwtUtil.getUserId(token),
+			jwtUtil.getLoginType(token));
+	}
+}
--- src/main/java/today/seasoning/seasoning/common/config/SecurityConfig.java
+++ src/main/java/today/seasoning/seasoning/common/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package today.seasoning.seasoning.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import today.seasoning.seasoning.common.util.JwtUtil;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-            .csrf(AbstractHttpConfigurer::disable)
-            .cors().and();
-        return httpSecurity.build();
-    }
+	private final JwtUtil jwtUtil;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors().and()
+			.addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+		return httpSecurity.build();
+	}
 }
