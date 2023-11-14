4f219b8bcfdf2bf44378247433d7330a2e6123a8
linavell <linavell@naver.com>
2023-11-14 19:44:22 +0900

    fix: 사용자 서비스 구현 수정

--- src/main/java/today/seasoning/seasoning/user/service/UserService.java
+++ src/main/java/today/seasoning/seasoning/user/service/UserService.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Service;
 import today.seasoning.seasoning.common.exception.CustomException;
 import today.seasoning.seasoning.user.domain.User;
 import today.seasoning.seasoning.user.domain.UserRepository;
-import today.seasoning.seasoning.user.dto.UserProfile;
+import today.seasoning.seasoning.user.dto.GetUserProfile;
+import today.seasoning.seasoning.user.dto.UpdateUserProfile;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +15,9 @@ public class UserService {
     private final UserRepository userRepository;
 
     // 프로필 조회
-    public UserProfile findUserProfile(Long id) {
+    public GetUserProfile findUserProfile(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "사용자 정보 조회 실패"));
-        return new UserProfile(user);
+        return new GetUserProfile(user);
     }
+
 }
