610177b64e584f9fdc0b009851fd398b68851638
linavell <linavell@naver.com>
2023-11-14 19:22:43 +0900

    feat: 사용자 서비스 구현

--- src/main/java/today/seasoning/seasoning/user/dto/UserProfile.java
+++ src/main/java/today/seasoning/seasoning/user/dto/UserProfile.java
@@ -2,6 +2,7 @@ package today.seasoning.seasoning.user.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import today.seasoning.seasoning.user.domain.User;
 
 @Getter
 @RequiredArgsConstructor
@@ -10,4 +11,10 @@ public class UserProfile {
 	private final String nickname;
 	private final String email;
 	private final String profileImageUrl;
+
+	public UserProfile(User user) {
+		this.nickname = user.getNickname();
+		this.email = user.getEmail();
+		this.profileImageUrl = user.getProfileImageUrl();
+	}
 }
