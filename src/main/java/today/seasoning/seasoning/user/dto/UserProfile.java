43743194c6beb310eedf25baf387ad0d47c4c146
linavell <linavell@naver.com>
2023-11-14 19:22:59 +0900

    feat: 사용자 정보 조회 웹 API 구현

--- src/main/java/today/seasoning/seasoning/user/dto/UserProfile.java
+++ src/main/java/today/seasoning/seasoning/user/dto/UserProfile.java
@@ -2,19 +2,16 @@ package today.seasoning.seasoning.user.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import today.seasoning.seasoning.user.domain.User;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
 public class UserProfile {
 
-	private final String nickname;
-	private final String email;
-	private final String profileImageUrl;
+    private final String nickname;
+    private final String email;
+    private final String profileImageUrl;
 
-	public UserProfile(User user) {
-		this.nickname = user.getNickname();
-		this.email = user.getEmail();
-		this.profileImageUrl = user.getProfileImageUrl();
-	}
 }
