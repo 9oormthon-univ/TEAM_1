3090a2cd199b2dfcc9795f001b7a72734e40cdb3
csct3434 <csct3434@gmail.com>
2023-11-15 05:32:41 +0900

    feat: 친구 목록 조회 웹 API 구현

--- src/main/java/today/seasoning/seasoning/friendship/controller/FriendshipController.java
+++ src/main/java/today/seasoning/seasoning/friendship/controller/FriendshipController.java
@@ -1,14 +1,18 @@
 package today.seasoning.seasoning.friendship.controller;
 
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import today.seasoning.seasoning.common.UserPrincipal;
+import today.seasoning.seasoning.friendship.dto.FindUserFriendsResult;
 import today.seasoning.seasoning.friendship.dto.ToUserAccountIdDto;
+import today.seasoning.seasoning.friendship.service.FindUserFriendsService;
 import today.seasoning.seasoning.friendship.service.RequestFriendshipService;
 
 @RequestMapping("/friend")
@@ -17,6 +21,7 @@ import today.seasoning.seasoning.friendship.service.RequestFriendshipService;
 public class FriendshipController {
 
 	private final RequestFriendshipService requestFriendshipService;
+	private final FindUserFriendsService findUserFriendsService;
 
 	@RequestMapping("/add")
 	public ResponseEntity<String> requestFriendship(
@@ -31,4 +36,14 @@ public class FriendshipController {
 		return ResponseEntity.ok().body("신청 완료");
 	}
 
+	@GetMapping("/list")
+	public ResponseEntity<List<FindUserFriendsResult>> findUserFriends(
+		@AuthenticationPrincipal UserPrincipal principal) {
+
+		Long userId = principal.getId();
+
+		List<FindUserFriendsResult> findUserFriendResults = findUserFriendsService.doFind(userId);
+
+		return ResponseEntity.ok().body(findUserFriendResults);
+	}
 }
