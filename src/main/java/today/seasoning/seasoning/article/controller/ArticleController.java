d437121cef8383bb0cdca438fbf528811119ed53
csct3434 <csct3434@gmail.com>
2023-11-16 03:07:25 +0900

    feat: 기록장 수정 웹 API 구현

--- src/main/java/today/seasoning/seasoning/article/controller/ArticleController.java
+++ src/main/java/today/seasoning/seasoning/article/controller/ArticleController.java
@@ -1,56 +1,69 @@
 package today.seasoning.seasoning.article.controller;
 
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import today.seasoning.seasoning.article.dto.FindArticleResult;
 import today.seasoning.seasoning.article.dto.RegisterArticleCommand;
 import today.seasoning.seasoning.article.dto.RegisterArticleWebRequest;
+import today.seasoning.seasoning.article.dto.UpdateArticleDto;
 import today.seasoning.seasoning.article.service.FindArticleService;
 import today.seasoning.seasoning.article.service.RegisterArticleService;
+import today.seasoning.seasoning.article.service.UpdateArticleService;
 import today.seasoning.seasoning.common.UserPrincipal;
 import today.seasoning.seasoning.common.util.TsidUtil;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/article")
 public class ArticleController {
 
-	private final RegisterArticleService registerArticleService;
-	private final FindArticleService findArticleService;
+    private final RegisterArticleService registerArticleService;
+    private final FindArticleService findArticleService;
+    private final UpdateArticleService updateArticleService;
 
-	@PostMapping
-	public ResponseEntity<String> registerArticle(@AuthenticationPrincipal UserPrincipal principal,
-		@RequestBody @Valid RegisterArticleWebRequest webRequest) {
+    @PostMapping
+    public ResponseEntity<String> registerArticle(@AuthenticationPrincipal UserPrincipal principal,
+                                                  @RequestBody @Valid RegisterArticleWebRequest webRequest) {
 
-		RegisterArticleCommand command = new RegisterArticleCommand(principal.getId(),
-			webRequest.getIsPublic(),
-			webRequest.getContents(),
-			webRequest.getImages());
+        RegisterArticleCommand command = new RegisterArticleCommand(principal.getId(),
+                webRequest.getIsPublic(),
+                webRequest.getContents(),
+                webRequest.getImages());
 
-		Long articleId = registerArticleService.doRegister(command);
+        Long articleId = registerArticleService.doRegister(command);
 
-		String stringArticleId = TsidUtil.toString(articleId);
-		return ResponseEntity.ok(stringArticleId);
-	}
+        String stringArticleId = TsidUtil.toString(articleId);
+        return ResponseEntity.ok(stringArticleId);
+    }
 
-	@GetMapping("/{stringArticleId}")
-	public ResponseEntity<FindArticleResult> findArticle(
-		@AuthenticationPrincipal UserPrincipal principal,
-		@PathVariable String stringArticleId) {
+    @GetMapping("/{stringArticleId}")
+    public ResponseEntity<FindArticleResult> findArticle(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @PathVariable String stringArticleId) {
 
-		Long userId = principal.getId();
-		Long articleId = TsidUtil.toLong(stringArticleId);
+        Long userId = principal.getId();
+        Long articleId = TsidUtil.toLong(stringArticleId);
 
-		FindArticleResult findArticleResult = findArticleService.doFind(userId, articleId);
+        FindArticleResult findArticleResult = findArticleService.doFind(userId, articleId);
 
-		return ResponseEntity.ok(findArticleResult);
-	}
+        return ResponseEntity.ok(findArticleResult);
+    }
+
+    @PutMapping("/{stringArticleId}")
+    public ResponseEntity<Void> updateArticle(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @PathVariable String stringArticleId,
+            @Valid @RequestBody UpdateArticleDto dto) {
+
+        Long userId = principal.getId();
+        Long articleId = TsidUtil.toLong(stringArticleId);
+
+        updateArticleService.doUpdate(userId, articleId, dto);
+
+        return ResponseEntity.ok().build();
+    }
 }
