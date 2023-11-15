22bf0a270df86606709b5edebb90d72809c92cbf
linavell <linavell@naver.com>
2023-11-16 03:00:29 +0900

    feat: 친구 끊기 웹 API 작성

--- src/main/java/today/seasoning/seasoning/friendship/controller/FriendshipController.java
+++ src/main/java/today/seasoning/seasoning/friendship/controller/FriendshipController.java
@@ -2,6 +2,7 @@ package today.seasoning.seasoning.friendship.controller;
 
 import java.util.List;
 import javax.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,38 +17,39 @@ import today.seasoning.seasoning.friendship.service.*;
 @RequiredArgsConstructor
 public class FriendshipController {
 
-	private final RequestFriendshipService requestFriendshipService;
-	private final FindUserFriendsService findUserFriendsService;
+    private final RequestFriendshipService requestFriendshipService;
+    private final FindUserFriendsService findUserFriendsService;
     private final AcceptFriendshipService acceptFriendshipService;
-	private final CancelFriendshipService cancelFriendshipService;
-	private final DeclineFriendshipService declineFriendshipService;
+    private final CancelFriendshipService cancelFriendshipService;
+    private final DeclineFriendshipService declineFriendshipService;
+    private final DeleteFriendshipService deleteFriendshipService;
 
-	@RequestMapping("/add")
-	public ResponseEntity<String> requestFriendship(
-		@AuthenticationPrincipal UserPrincipal principal,
-		@Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
+    @RequestMapping("/add")
+    public ResponseEntity<String> requestFriendship(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
 
-		Long fromUserId = principal.getId();
-		String toUserAccountId = toUserAccountIdDto.getAccountId();
+        Long fromUserId = principal.getId();
+        String toUserAccountId = toUserAccountIdDto.getAccountId();
 
-		requestFriendshipService.doService(fromUserId, toUserAccountId);
+        requestFriendshipService.doService(fromUserId, toUserAccountId);
 
-		return ResponseEntity.ok().body("신청 완료");
-	}
+        return ResponseEntity.ok().body("신청 완료");
+    }
 
-	@GetMapping("/list")
-	public ResponseEntity<List<FindUserFriendsResult>> findUserFriends(
-		@AuthenticationPrincipal UserPrincipal principal) {
+    @GetMapping("/list")
+    public ResponseEntity<List<FindUserFriendsResult>> findUserFriends(
+            @AuthenticationPrincipal UserPrincipal principal) {
 
-		Long userId = principal.getId();
+        Long userId = principal.getId();
 
-		List<FindUserFriendsResult> findUserFriendResults = findUserFriendsService.doFind(userId);
+        List<FindUserFriendsResult> findUserFriendResults = findUserFriendsService.doFind(userId);
 
-		return ResponseEntity.ok().body(findUserFriendResults);
-	}
+        return ResponseEntity.ok().body(findUserFriendResults);
+    }
 
-	@PutMapping("/add/accept")
-	public ResponseEntity<String> acceptFriendship(
+    @PutMapping("/add/accept")
+    public ResponseEntity<String> acceptFriendship(
             @AuthenticationPrincipal UserPrincipal principal,
             @Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
 
@@ -57,31 +59,44 @@ public class FriendshipController {
         acceptFriendshipService.doService(userId, requesterAccountId);
 
         return ResponseEntity.ok().body("수락 완료");
-	}
+    }
 
-	@DeleteMapping("/add/cancel")
-	public ResponseEntity<String> cacelFriendship(
-			@AuthenticationPrincipal UserPrincipal principal,
-			@Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
+    @DeleteMapping("/add/cancel")
+    public ResponseEntity<String> cancelFriendship(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
 
-		Long userId = principal.getId();
-		String toUserAccountId = toUserAccountIdDto.getAccountId();
+        Long userId = principal.getId();
+        String toUserAccountId = toUserAccountIdDto.getAccountId();
 
-		cancelFriendshipService.doService(userId, toUserAccountId);
+        cancelFriendshipService.doService(userId, toUserAccountId);
 
-		return ResponseEntity.ok().body("취소 완료");
-	}
+        return ResponseEntity.ok().body("취소 완료");
+    }
 
-	@DeleteMapping("/add/decline")
-	public ResponseEntity<String> declineFriendship(
-			@AuthenticationPrincipal UserPrincipal principal,
-			@Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
+    @DeleteMapping("/add/decline")
+    public ResponseEntity<String> declineFriendship(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
 
-		Long userId = principal.getId();
-		String toUserAccountId = toUserAccountIdDto.getAccountId();
+        Long userId = principal.getId();
+        String toUserAccountId = toUserAccountIdDto.getAccountId();
 
-		declineFriendshipService.doService(userId, toUserAccountId);
+        declineFriendshipService.doService(userId, toUserAccountId);
 
-		return ResponseEntity.ok().body("거절 완료");
-	}
+        return ResponseEntity.ok().body("거절 완료");
+    }
+
+    @DeleteMapping("/unfriend")
+    public ResponseEntity<String> deleteFriendship(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @Valid @RequestBody ToUserAccountIdDto toUserAccountIdDto) {
+
+        Long userId = principal.getId();
+        String toUserAccountId = toUserAccountIdDto.getAccountId();
+
+        deleteFriendshipService.doService(userId, toUserAccountId);
+
+        return ResponseEntity.ok().body("삭제 완료");
+    }
 }
