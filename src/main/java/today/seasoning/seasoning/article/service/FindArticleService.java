3025da63798a4083ccf842a2931cd28ba652f1e8
csct3434 <csct3434@gmail.com>
2023-11-15 17:52:43 +0900

    feat: 기록장 조회 서비스 구현
    - 조회 권한 검증 : 자신의 글 또는 친구의 공개된 글만 조회 가능

---
+++ src/main/java/today/seasoning/seasoning/article/service/FindArticleService.java
@@ -0,0 +1,78 @@
+package today.seasoning.seasoning.article.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import today.seasoning.seasoning.article.domain.Article;
+import today.seasoning.seasoning.article.domain.ArticleLike;
+import today.seasoning.seasoning.article.domain.ArticleRepository;
+import today.seasoning.seasoning.article.dto.FindArticleImageResult;
+import today.seasoning.seasoning.article.dto.FindArticleResult;
+import today.seasoning.seasoning.common.exception.CustomException;
+import today.seasoning.seasoning.friendship.service.port.in.CheckFriendshipValid;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FindArticleService {
+
+	private final ArticleRepository articleRepository;
+	private final CheckFriendshipValid checkFriendshipValid;
+
+	public FindArticleResult doFind(Long userId, Long articleId) {
+		Article article = articleRepository.findById(articleId)
+			.orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "기록장 조회 실패"));
+
+		validatePermission(userId, article);
+
+		return createFindResult(userId, article);
+	}
+
+	private void validatePermission(Long userId, Article article) {
+		Long authorId = article.getUser().getId();
+
+		// 자신의 글
+		if (authorId.equals(userId)) {
+			return;
+		}
+		// 공개된 친구의 글
+		if (article.isPublic() && checkFriendshipValid.doCheck(userId, authorId)) {
+			return;
+		}
+
+		throw new CustomException(HttpStatus.FORBIDDEN, "조회 권한 없음");
+	}
+
+	private FindArticleResult createFindResult(Long userId, Article article) {
+		List<FindArticleImageResult> findArticleImageResults
+			= createFindArticleImageResults(article);
+
+		boolean userLikesArticle = checkUserLikesArticle(userId, article);
+
+		return new FindArticleResult(
+			article.getCreatedYear(),
+			article.getCreatedTerm(),
+			article.getContents(),
+			findArticleImageResults,
+			article.getArticleLikes().size(),
+			userLikesArticle);
+	}
+
+	private List<FindArticleImageResult> createFindArticleImageResults(Article article) {
+		return article.getArticleImages()
+			.stream()
+			.map(FindArticleImageResult::build)
+			.collect(Collectors.toList());
+	}
+
+	private boolean checkUserLikesArticle(Long userId, Article article) {
+		return article.getArticleLikes()
+			.stream()
+			.map(ArticleLike::getUser)
+			.anyMatch(user -> user.getId().equals(userId));
+	}
+
+}
