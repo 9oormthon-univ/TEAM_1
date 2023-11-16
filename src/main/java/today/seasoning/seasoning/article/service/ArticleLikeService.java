069b0b46db49a6dcf4449b5a2c0ef7745c89642a
csct3434 <csct3434@gmail.com>
2023-11-17 03:55:04 +0900

    feat: 기록장 좋아요 권한 검증 로직 추가

--- src/main/java/today/seasoning/seasoning/article/service/ArticleLikeService.java
+++ src/main/java/today/seasoning/seasoning/article/service/ArticleLikeService.java
@@ -9,6 +9,7 @@ import today.seasoning.seasoning.article.domain.ArticleLike;
 import today.seasoning.seasoning.article.domain.ArticleLikeRepository;
 import today.seasoning.seasoning.article.domain.ArticleRepository;
 import today.seasoning.seasoning.common.exception.CustomException;
+import today.seasoning.seasoning.friendship.service.port.in.CheckFriendshipValid;
 import today.seasoning.seasoning.user.domain.User;
 import today.seasoning.seasoning.user.domain.UserRepository;
 
@@ -20,11 +21,14 @@ public class ArticleLikeService {
 	private final UserRepository userRepository;
 	private final ArticleRepository articleRepository;
 	private final ArticleLikeRepository articleLikeRepository;
+	private final CheckFriendshipValid checkFriendshipValid;
 
 	public void doLike(Long userId, Long articleId) {
 		User user = userRepository.findById(userId).get();
 
 		Article article = findArticleOrThrow(articleId);
+
+		validatePermission(userId, article);
 
 		if (articleLikeRepository.findByArticleAndUser(articleId, userId).isPresent()) {
 			throw new CustomException(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다.");
@@ -35,7 +39,9 @@ public class ArticleLikeService {
 	}
 
 	public void cancelLike(Long userId, Long articleId) {
-		findArticleOrThrow(articleId);
+		Article article = findArticleOrThrow(articleId);
+
+		validatePermission(userId, article);
 
 		ArticleLike articleLike = articleLikeRepository.findByArticleAndUser(articleId, userId)
 			.orElseThrow(() -> new CustomException(HttpStatus.FORBIDDEN, "좋아요를 누르지 않았습니다"));
@@ -46,5 +52,20 @@ public class ArticleLikeService {
 	private Article findArticleOrThrow(Long articleId) {
 		return articleRepository.findById(articleId)
 			.orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "기록장 조회 실패"));
+	}
+
+	private void validatePermission(Long userId, Article article) {
+		Long authorId = article.getUser().getId();
+
+		// 자신의 글
+		if (authorId.equals(userId)) {
+			return;
+		}
+		// 공개된 친구의 글
+		if (article.isPublished() && checkFriendshipValid.doCheck(userId, authorId)) {
+			return;
+		}
+
+		throw new CustomException(HttpStatus.FORBIDDEN, "접근 권한 없음");
 	}
 }
