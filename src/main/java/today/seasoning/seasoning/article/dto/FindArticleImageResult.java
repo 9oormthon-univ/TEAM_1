037c494b601b584a7518d281d5c7e22e4b6e9873
csct3434 <csct3434@gmail.com>
2023-11-16 19:38:46 +0900

    feat: 불필요한 기록장 조회 응답 속성 제거
    이미지의 id는 서비스 외부에서 사용되지 않으므로 제거

--- src/main/java/today/seasoning/seasoning/article/dto/FindArticleImageResult.java
+++ src/main/java/today/seasoning/seasoning/article/dto/FindArticleImageResult.java
@@ -3,19 +3,16 @@ package today.seasoning.seasoning.article.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import today.seasoning.seasoning.article.domain.ArticleImage;
-import today.seasoning.seasoning.common.util.TsidUtil;
 
 @Getter
 @RequiredArgsConstructor
 public class FindArticleImageResult {
 
-	private final String id;
 	private final int sequence;
 	private final String url;
 
 	public static FindArticleImageResult build(ArticleImage articleImage) {
 		return new FindArticleImageResult(
-			TsidUtil.toString(articleImage.getId()),
 			articleImage.getSequence(),
 			articleImage.getUrl());
 	}
